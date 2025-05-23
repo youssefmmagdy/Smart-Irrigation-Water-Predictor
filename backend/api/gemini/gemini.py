1e1b32c37c889d7722df546e3cacad6e497885d7
Yusuf <youssefmmagdy55@gmail.com>
2025-05-23 20:36:43 +0200

    Last Commit

--- backend/api/gemini/gemini.py
+++ backend/api/gemini/gemini.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 from llama_index.core import SimpleDirectoryReader
 from llama_index.core import VectorStoreIndex
-from llama_index.llms.gemini import Gemini
+from llama_index.llms.google_genai import GoogleGenAI
 
 from llama_index.embeddings.google_genai import GoogleGenAIEmbedding
 from llama_index.core import Settings
@@ -11,8 +11,8 @@ from llama_index.core.node_parser import SentenceSplitter
 load_dotenv()
 google_api_key = os.getenv("GOOGLE_API_KEY")
 
-documents = SimpleDirectoryReader('./data').load_data()
-model = Gemini(model='gemini-2.5-flash-preview-05-20', api_key=google_api_key)
+documents = SimpleDirectoryReader('api/data').load_data()
+model = GoogleGenAI(model='gemini-2.5-flash-preview-05-20', api_key=google_api_key)
 model_embedding = GoogleGenAIEmbedding(model_name='text-embedding-004')
 
 Settings.llm = model
@@ -33,4 +33,4 @@ def ask_gemini(input_query):
     response = query_engine.query(input_query)
     return response.response
 
-print(ask_gemini("Who is the author of the thesis?"))
+print(ask_gemini("What is the outline of the thesis?"))
